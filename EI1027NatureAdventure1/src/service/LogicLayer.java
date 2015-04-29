e6ebd235fd436a35e5f1fd982ee3f8763ba8ceab
serrano94 <al259368@uji.es>
2015-04-29 19:08:38 +0200

    Added some methods relationed with instructor in logicLayer

--- EI1027NatureAdventure1/src/service/LogicLayer.java
+++ EI1027NatureAdventure1/src/service/LogicLayer.java
@@ -1,14 +1,19 @@
 package service;
 
+import java.util.Collection;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import classes.Instructor;
 import database.DaoActivity;
 import database.DaoBooking;
 import database.DaoClient;
 import database.DaoInstructor;
 import database.DaoStatus;
 
+//TODO Cambiar referencias daos viejos
 //Implementación palera 
 //TODO trabajar en memoria
 //TODO guardar en fichero local
@@ -28,7 +33,7 @@ public class LogicLayer {
 	private int innerBookingID;
 	private int activeBookingID;
 	private int activityID;
-	
+	private int instructorID;
 	
 	/**
 	 * Inicializa las IDs para que se puedan autoincrementar.
@@ -45,8 +50,79 @@ public class LogicLayer {
 		this.innerBookingID = daoBooking.getMaxInnerID();
 		this.activeBookingID = daoBooking.getMaxActiveID();
 		this.activityID = daoActivity.getMaxID();
+		this.instructorID = daoInstructor.getMaxID();
 	}
 
+	
+	/**
+	 * ALPHA VERSION
+	 */
+	
+	
+	/**
+	 * INSTRUCTOR ZONE
+	 */
+	
+	
+	/**
+	 * Add a instructor in the database.
+	 */
+	public void addInstructor(Instructor instructor){
+		DaoInstructor miDao = (DaoInstructor) daoInstructor; //Casting preparado para cuando se establezca el cambio a abstract
+													   //Como en este momento no tengo la interfaz delante lo dejo asi pendiente de cambiar el método
+		miDao.addInstructor(instructor);
+		
+	}
+	
+	/**
+	 * Delete a instructor from the database. The ssNumber is required
+	 */
+	public void deleteInstructor(String code){
+		DaoInstructor miDao = (DaoInstructor) daoInstructor;
+		if (this.getInstructor(code)==null){
+			return;
+		}
+		miDao.deleteInstructor(code);
+	}
+	
+	/**
+	 * Update a instructor from the database. This operation is only allowed when the instructor was registered before in the database
+	 */
+	
+	public void updateInstructor(Instructor instructor){
+		DaoInstructor miDao = (DaoInstructor) daoInstructor; 
+		if (this.getInstructor(instructor.getSsNumber())==null){
+			return;
+		}
+		miDao.updateInstructor(instructor);
+	}
+
+	/**
+	 * Given a ssNumber a Instructor is delivered. If the instructor doesn't exist in the database it returns null 
+	 *@return a instructor or null
+	 */
+	
+	public Instructor getInstructor(String code){
+		DaoInstructor miDao = (DaoInstructor) daoInstructor; 
+		Instructor myInstructor = miDao.getInstructor(code);
+		return myInstructor;
+	}
+	
+	/**
+	 * Get all the instructors in the database
+	 * @return A collection of Instructor with all instructors
+	 */
+	
+	public Collection<Instructor> getAllInstructors(){ //Devuelvo solo lista de Instructores para facilitar tarea a la vista
+		DaoInstructor miDao = (DaoInstructor) daoInstructor; 
+		Map<String,Instructor> allInstructors =  miDao.getInstructors();
+		Collection<Instructor> allInstructorsClasses= allInstructors.values();
+		return allInstructorsClasses;
+	}
+	
+	
+		
+	
 	
 	
 	
