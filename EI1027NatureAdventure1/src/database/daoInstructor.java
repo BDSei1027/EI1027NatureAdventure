b298f73c842b32776ce06b279173fe85ffd6e89d
RataDP <ratadp@gmail.com>
2015-05-05 11:52:41 +0200

    New methods deleteActivity/ies, getElements and Javadocs comments
    He añadido los nuevos metodos que faltaban, y he comentado todos lo
    metodos con javadocs.
    FIJARSE QUE REMOVEELEMENT YA QUITA DE INSTRUIDAS
    
    Signed-off-by: RataDP <ratadp@gmail.com>

--- EI1027NatureAdventure1/src/database/daoInstructor.java
+++ EI1027NatureAdventure1/src/database/daoInstructor.java
@@ -2,7 +2,9 @@ package database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -12,7 +14,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import classes.Instructor;
-//TODO getMaxID, deleteInstructorFromActivities getElements
+
 @Repository
 public class daoInstructor implements DaoInterface {
 
@@ -23,6 +25,9 @@ public class daoInstructor implements DaoInterface {
 		this.dataSource = new JdbcTemplate(datasource);
 	}
 	
+	/*
+	 * RowMapper for the class Instructor
+	 */
 	private static final class InstructorMapper implements RowMapper<Instructor> {
 		public Instructor mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Instructor instructor = new Instructor();
@@ -41,7 +46,12 @@ public class daoInstructor implements DaoInterface {
 		}
 	}
 	
-	
+	/**
+	 * Method to add an Instructor into the DB
+	 * @see database.DaoInterface#addElement(java.lang.Object)
+	 * @param element Instructor, class Instructor
+	 * TODO Comprobar que lo que le pasamos es un Instructor
+	 */
 	@Override
 	public void addElement(Object element) {
 		Instructor instr = (Instructor) element;
@@ -49,16 +59,28 @@ public class daoInstructor implements DaoInterface {
 							+ "values(?, ?, ?, ?, ?, ?,?)";
 		dataSource.update(sql, instr.getSsNumber(), instr.getIdNumber(), instr.getName(), instr.getLastName(), instr.getEmail(), instr.getTelephone(), instr.isActive());
 		
-		//TODO Mirar en instruidas
+		addActivities(instr.getSsNumber(), instr.getActivities());
 	}
 
+	/**
+	 * Method to remove an Instructor from the DB
+	 * @see database.DaoInterface#deleteElement(java.lang.Object)
+	 * @param element String with the ssNumber
+	 */
 	@Override
 	public void deleteElement(Object element) {
 		String id = (String) element;
 		String sql = "DELETE FROM instructor WHERE ssNumber = ?";
 		dataSource.update(sql, id);
+		deleteInstructorFromActivities(id);
 	}
 
+	/**
+	 * Method to update an Instructor in the DB
+	 * @see database.DaoInterface#updateElement(java.lang.Object)
+	 * @param element Instructor, class Instructor
+	 * TODO Comprobar que es de la clase instructor
+	 */
 	@Override
 	public void updateElement(Object element) {
 		Instructor instr = (Instructor) element;
@@ -68,6 +90,12 @@ public class daoInstructor implements DaoInterface {
 		dataSource.update(sql, instr.getIdNumber(), instr.getName(),instr.getLastName(), instr.getEmail(), instr.getTelephone(), instr.isActive(), instr.getSsNumber());
 	}
 
+	/**
+	 * Method to obtain an Instructor from the DB
+	 * @see database.DaoInterface#getElement(java.lang.Object)
+	 * @param identifier String with the ssNumber
+	 * @return an Instructor with all the field, includes Activities
+	 */
 	@Override
 	public Object getElement(Object identifier) {
 		String id = (String) identifier;
@@ -77,24 +105,50 @@ public class daoInstructor implements DaoInterface {
 		return instructor;
 	}
 
+	/**
+	 * Method to obtain all the instructors from the DB
+	 * @see database.DaoInterface#getElements()
+	 * @return Map<String, Instructor>, key: ssnumber, value: Instructor with all the fields, includes Activities
+	 */
 	@Override
 	public Object getElements() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM instructor";
+		Map<String, Instructor> map = new HashMap<String, Instructor>();
+		List<Instructor> list = dataSource.query(sql, new InstructorMapper());
+		for(Instructor i: list) {
+			i.setActivities(getActivitiesInstructor(i.getSsNumber()));
+			map.put(i.getSsNumber(), i);
+		}
+		return map;
 	}
 
+	/**
+	 * Method to get the activities that the instructor can supervise
+	 * @param id String with the ssnumber
+	 * @return List<Integer> with the idact of the activities
+	 */
 	private List<Integer> getActivitiesInstructor(String id) {
 		String sql = "SELECT idact FROM instruidas WHERE ssnumber = ?";
 		return (List<Integer>) dataSource.queryForList(sql, Integer.class, id);
 	}
 	
 	// TODO cambiamos el nombre?
+	/**
+	 * Method to add an activity that the instructor can supervise into instruidas
+	 * @param ssnum Instructor's identifier
+	 * @param idact Activity's identifier
+	 */
 	public void addActivity(String ssnum, int idact) {
 		String sql = "INSERT INTO instruidas(idact, ssnumber) VALUES(? ,?)";
 		dataSource.update(sql, idact, ssnum);
 	}
 	
 	// TODO cambiamos el nombre?
+	/**
+	 * Method to add some activities that the instructor can supervise into instruidas
+	 * @param ssnum Instructor's identifier
+	 * @param acts List<Integer> Activities' identifiers
+	 */
 	public void addActivities(String ssnum, List<Integer> acts) {
 		if (acts.size() != 0) {
 			StringBuilder sb = new StringBuilder("INSERT INTO instruidas(idact, ssnumber) VALUES ");
@@ -106,4 +160,22 @@ public class daoInstructor implements DaoInterface {
 		}
 	}
 	
+	/**
+	 * Method to remove an activity that the instructor can supervise
+	 * @param ssnum Instructor's identifier
+	 * @param idact Activity's identifier
+	 */
+	public void deleteInstructorFromActivity(String ssnum, int idact) {
+		String sql = "DELETE FROM instruidas WHERE ssnumber = ?, idact = ?";
+		dataSource.update(sql, ssnum, idact);
+	}
+	
+	/**
+	 * Method to remove all the activities that the instructor can supervise
+	 * @param ssnum Instructor's identifier
+	 */
+	public void deleteInstructorFromActivities(String ssnum) {
+		String sql = "DELETE FROM instruidas WHERE ssnumber = ?";
+		dataSource.update(sql, ssnum);
+	}
 }
