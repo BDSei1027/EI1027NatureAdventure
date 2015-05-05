7b7dadee11a175c61e21d3c6a3735b8624dde730
RataDP <ratadp@gmail.com>
2015-05-05 14:11:26 +0200

    Comentado con javadocs
    Signed-off-by: RataDP <ratadp@gmail.com>

--- EI1027NatureAdventure1/src/database/DaoInterface.java
+++ EI1027NatureAdventure1/src/database/DaoInterface.java
@@ -3,14 +3,35 @@ package database;
 
 public interface DaoInterface {
 
-	public abstract void addElement(Object element);
+	/**
+	 * Method to add an Object into de DB
+	 * @param element Object to add
+	 */
+	public  void addElement(Object element);
 	
-	public abstract void deleteElement(Object element);
+	/**
+	 * Method to remove an Object from the DB
+	 * @param element Identifier of the Object
+	 */
+	public void deleteElement(Object element);
 	
-	public abstract void updateElement(Object element);
+	/**
+	 * Method to update an Object in the DB
+	 * @param element Objecto to update
+	 */
+	public void updateElement(Object element);
 	
-	public abstract Object getElement(Object identifier);
+	/**
+	 * Method to obtain an Object from the DB
+	 * @param identifier Object's identifier
+	 * @return Object
+	 */
+	public Object getElement(Object identifier);
 	
-	public abstract Object getElements();
+	/**
+	 * Method to obtain all the Objects from the DB
+	 * @return Collections<Object>
+	 */
+	public Object getElements();
 	
 }
