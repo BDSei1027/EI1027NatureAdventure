f4912fb6592cc6a1d9eb08347d310fac6c062a07
al259317 <al259317@AULA1167ACT.sg.uji.es>
2015-05-06 17:00:50 +0200

    Maybe finished

--- EI1027NatureAdventure1/src/main/controller/IdentificationControler.java
+++ EI1027NatureAdventure1/src/main/controller/IdentificationControler.java
@@ -2,7 +2,6 @@ package main.controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import classes.User;
 import Validators.UserValidator;
 import service.LogicLayer;
 
@@ -20,11 +20,21 @@ public class IdentificationControler {
 	
 	private LogicLayer service;
 
+
+	/**
+	 * Method that allows automatic independence injection
+	 * @param service Service to inject
+	 */
 	@Autowired
 	public void setServicio(LogicLayer service){
 		this.service = service;
 	}
 	
+	/**
+	 * Method that initializes a new user used by the form
+	 * @param model Page model
+	 * @return Login page
+	 */
 	@RequestMapping(value="/login")
 	public String login(Model model){
 		model.addAttribute("user", new User());
@@ -32,32 +42,50 @@ public class IdentificationControler {
 		
 	}
 	
+	/**
+	 * Method used to process login form data
+	 * @param user User data gathered by the form
+	 * @param bindingResult Result of processing the form
+	 * @param session Http session used to store the user information
+	 * @return Page that requested the login or the main page
+	 */
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String processLogin(@ModelAttribute("lastPage") String lastPage, @ModelAttribute("user") User user, BindingResult bindingResult, HttpSession session){
+	public String processLogin(@ModelAttribute("user") User user, BindingResult bindingResult, HttpSession session){
 		//Correct field format validator
 		UserValidator userValidator = new UserValidator();
 		userValidator.validate(user, bindingResult);
 		
 		if(bindingResult.hasErrors()) return "login";
 		
-		user = service.loginUser(user.getName(), user.getPassword());
+		user = service.loginUser(user.getUser(), user.getPassword());
 		
 		//Password check 
 		if(user==null) {
 			bindingResult.rejectValue("password", "badpw", "Incorrect password"); 
-			return lastPage;
+			return "login";
 		}
 		
+		//Maintain the user data in the session
 		session.setAttribute("user", user);
 		
+		//Get the page that called the login
+		String nextPage = (String) session.getAttribute("nextPage");
+		
+		//Return the page that called the login or go to the main page
+		if(nextPage != null) return nextPage;
 		return "redirect:index.html";
 		
 	}
 	
 	
+	/**
+	 * Method used to logout the user
+	 * @param session Http session that contains the user data
+	 * @return Bye page
+	 */
 	@RequestMapping(value="/logout")
-	public String logout(@ModelAttribute("lastPage") String lastPage, HttpSession session){
+	public String logout(HttpSession session){
 		session.invalidate();
-		return lastPage;		
+		return "redirect:bye.html";		
 	}
 }
