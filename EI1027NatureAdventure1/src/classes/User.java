5fb9edd2a710cb75c0bacf27e2428fbb8d7cd436
RataDP <ratadp@gmail.com>
2015-05-06 16:14:50 +0200

    User y daoUser
    Signed-off-by: RataDP <ratadp@gmail.com>

---
+++ EI1027NatureAdventure1/src/classes/User.java
@@ -0,0 +1,41 @@
+package classes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	
+	private String user;
+	private String password;
+	private String type;
+
+	public User() {
+		super();
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
---
+++ EI1027NatureAdventure1/src/database/daoUser.java
@@ -0,0 +1,92 @@
+package database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import classes.User;
+
+@Repository
+public class daoUser implements DaoInterface {
+
+	private JdbcTemplate dataSource;
+	
+	@Autowired
+	private void setDataSource(DataSource datasource) {
+		this.dataSource = new JdbcTemplate(datasource);
+	}
+	
+	/*
+	 * RowMapper for the class User
+	 */
+	private final static class UserMapper implements RowMapper<User> {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			try {
+				user.setUser(rs.getString("user"));
+				user.setPassword(rs.getString("password"));
+				user.setType(rs.getString("type"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return user;
+		}
+		
+	}
+	
+	@Override
+	public void addElement(Object element) {
+		User u = (User) element;
+		String sql = "INSERT INTO User(user,password,type) VALUES(?, ?, ?);";
+		dataSource.update(sql, u.getUser(), u.getPassword(), u.getType());
+	}
+
+	@Override
+	public void deleteElement(Object element) {
+		String user = "";
+		if (element instanceof String)
+			user = (String) element;
+		else if (element instanceof User) {
+			User usuario = (User) element;
+			user = usuario.getUser();
+		}
+		String sql = "DELETE FROM User WHERE user = ?;";
+		dataSource.update(sql, user);
+
+	}
+
+	@Override
+	public void updateElement(Object element) {
+		User u = (User) element;
+		String sql = "UPDATE User SET password = ?, type = ? WHERE user = ?;";
+		dataSource.update(sql, u.getPassword(), u.getType(), u.getUser());
+
+	}
+
+	@Override
+	public Object getElement(Object identifier) {
+		String user = (String) identifier;
+		String sql = "SELECT * FROM user WHERE user = ?;";
+		return dataSource.queryForObject(sql, new UserMapper(), user);
+	}
+
+	@Override
+	public Object getElements() {
+		Map<String, User> map = new HashMap<String, User>();
+		String sql = "SELECT * FROM user;";
+		List<User> list = dataSource.query(sql, new UserMapper());
+		for (User u : list) map.put(u.getUser(), u);
+		return map;
+	}
+
+}
