809b73726862010090e01eaac813580fa041e78e
al259368 <al259368@aula1168act.sg.uji.es>
2015-05-06 17:09:23 +0200

    asdfasdf

---
+++ EI1027NatureAdventure1/src/validators/UserValidator.java
@@ -0,0 +1,27 @@
+package validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import classes.User;
+
+public class UserValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> cls) {
+		return User.class.equals(cls);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		User user = (User) obj;
+		
+		if (user.getUser().trim().equals("")){
+			errors.rejectValue("nombre", "obligatorio",
+                    "Es necesario introducir un valor");
+
+		}
+		
+	}
+
+}
