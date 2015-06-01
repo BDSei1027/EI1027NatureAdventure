822529daf11d714db0752bf50c2c393763200a27
serrano94 <al259368@uji.es>
2015-06-01 11:40:33 +0200

    asdasas

--- EI1027NatureAdventure1/src/validators/InstructorValidator.java
+++ EI1027NatureAdventure1/src/validators/InstructorValidator.java
@@ -3,6 +3,8 @@ package validators;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import com.aeat.valida.Validador;
+
 import classes.Instructor;
 import classes.User;
 
@@ -16,28 +18,26 @@ public class InstructorValidator implements Validator {
 	@Override
 	public void validate(Object obj, Errors errors) {
 		Instructor instructor = (Instructor) obj;
+		
 		if(instructor.getSsNumber().trim().length() <= 9){
-			errors.rejectValue("ssNumber","longitud","La longitud debe ser mayor que 9 carácteres"); 
-		}
-		if(instructor.getIdNumber().trim().length()!=9){
-			errors.rejectValue("idNumber", "digitos","La longitud debe ser de 9 carácteres");
+			errors.rejectValue("ssNumber","longitud","La longitud debe ser mayor que 9 caracteres"); 
 		}
 		
 		if(instructor.getName().trim().equals("")){
-			errors.rejectValue("name", "nameContent","Este campo no puede estar vacío");
+			errors.rejectValue("name", "nameContent","Este campo no puede estar vacio");
 		}
 		
 		if(instructor.getLastName().trim().equals("")){
-			errors.rejectValue("lastName", "lastNameContent","Este campo no puede estar vacío");
+			errors.rejectValue("lastName", "lastNameContent","Este campo no puede estar vacio");
 		}
 		
 		if(!instructor.getEmail().matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
 			errors.rejectValue("email", "emailAdrress","Debes introducir un email valido");
 		}
-		
-		if(!instructor.getIdNumber().matches( "")){
-			
+		Validador validadorAgenciaTributaria = new Validador();
+		if(validadorAgenciaTributaria.checkNif(instructor.getIdNumber().toUpperCase())<0){
+			errors.rejectValue("idNumber", "digitos","El idNumber no es correcto");
 		}
 	}
 
