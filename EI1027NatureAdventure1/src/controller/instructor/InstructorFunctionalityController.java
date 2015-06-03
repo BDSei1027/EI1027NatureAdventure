c97252f8e0c59e5f9de74305ded7e870c317bad8
Demils <Demils@Demils-PC>
2015-06-03 19:12:37 +0200

    fallosparapej

--- EI1027NatureAdventure1/src/controller/instructor/InstructorFunctionalityController.java
+++ EI1027NatureAdventure1/src/controller/instructor/InstructorFunctionalityController.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import controller.basics.AbstractController;
 import classes.Activity;
+import classes.Booking;
 import classes.Instructor;
 import classes.User;
 import validators.UserValidator;
@@ -21,18 +24,8 @@ import service.LogicLayer;
 
 
 @Controller
-public class InstructorFunctionalityController {
-	
-	private LogicLayer service;
-
-
-	/**
-	 * Method that allows automatic independence injection
-	 * @param service Service to inject
-	 */
-	public void setService(LogicLayer service){
-		this.service = service;
-	}
+@RequestMapping(value="/instructor")
+public class InstructorFunctionalityController extends AbstractController{
 	
 	
 	/**
@@ -41,17 +34,11 @@ public class InstructorFunctionalityController {
 	 * @param session Session of the user
 	 * @return Main monitors page
 	 */
-	@RequestMapping(value="/instructor")
+	@RequestMapping
 	public String monitorPage(Model model, HttpSession session){
-		//If the user is not logged
-		User user = (User) session.getAttribute("user");
-		if(user == null) return "redirect:login.jsp";
-		
-		//If the user is not an instructor
-		if(user.getType() != 1) return "restricted";
-		
+
 		//Get the activities of the instructor
-		Instructor instructor = service.getInstructor(user);
+		Instructor instructor = service.getInstructor((User) session.getAttribute("user"));
 		Collection<Activity> instructorActivities = service.getAllActivities(instructor);
 		
 		//Attach the list to the model
@@ -59,5 +46,23 @@ public class InstructorFunctionalityController {
 		return "instructor";
 		
 	}
-
+	
+	@RequestMapping(value="/details/{idBooking}")
+	public String detailsPage(@PathVariable int idBooking, Model model){
+		Booking booking = service.getBooking(idBooking);
+		
+		model.addAttribute("booking", booking);
+		model.addAttribute("activity", service.getActivity(booking.getIdAct()));
+		model.addAttribute("client", service.getClient(booking.getClientId()));
+		
+		return "instructor/details";
+	}
+	
+	@RequestMapping(value="main/history")
+	public String pastBookings(Model model, HttpSession session){
+		Instructor instructor = service.getInstructor((User) session.getAttribute("user"));
+		
+		model.addAttribute("bookings", service.getPastBookings(instructor));
+		return "main/history";
+	}
 }
