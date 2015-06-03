2858ed037d634c1a26fcd2def7d0a776baf4fe3d
Demils <Demils@Demils-PC>
2015-06-03 22:26:37 +0200

    major

---
+++ EI1027NatureAdventure1/src/controller/admin/UserManagement.java
@@ -0,0 +1,27 @@
+package controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import controller.basics.AbstractController;
+
+
+@Controller
+@RequestMapping("/admin/userManagement")
+public class UserManagement extends AbstractController {	
+	
+
+	@RequestMapping
+	public String userPage(Model model){
+		model.addAttribute("userList", service.getAllUsers());
+		return "admin/userManagement";
+	}
+	
+	@RequestMapping(value="delete/{userName}")
+	public String removeUser(@PathVariable String userName, Model model){
+		service.removeUser(userName);
+		return "redirect:/admin/userManagement";
+	}
+}
