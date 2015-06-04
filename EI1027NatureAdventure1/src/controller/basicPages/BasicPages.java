cd78bae64c709f772ede4e556621e64fe71825a6
Demils <Demils@Demils-PC>
2015-06-04 16:03:50 +0200

    Controller index

---
+++ EI1027NatureAdventure1/src/controller/basicPages/BasicPages.java
@@ -0,0 +1,16 @@
+package controller.basicPages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import controller.basics.AbstractController;
+
+
+@Controller
+public class BasicPages extends AbstractController {	
+
+	@RequestMapping(value="/index")
+	public String indexPage(){
+		return "index";
+	}
+}
