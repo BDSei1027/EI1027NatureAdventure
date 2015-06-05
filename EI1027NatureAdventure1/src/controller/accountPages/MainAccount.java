3693d0e461cbd4248cd0c378084cfc0f1fadaab3
Demils <Demils@192.168.1.252>
2015-06-06 00:48:27 +0200

    acc done

--- EI1027NatureAdventure1/src/controller/accountPages/MainAccount.java
+++ EI1027NatureAdventure1/src/controller/accountPages/MainAccount.java
@@ -1,7 +1,9 @@
 package controller.accountPages;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -31,64 +33,91 @@ public class MainAccount extends AbstractController{
 	 * @return The page with the user data
 	 */
 	@RequestMapping
-	public String userPage(Model model, HttpSession session){
+	public String acoountPage(Model model, HttpSession session){
 		User user = (User) session.getAttribute("user");
 		
-		if (user.getType() == 0) {
-			return "redirect:/admin";
-		} else if (user.getType() == 1) {
-			Instructor instr = service.getInstructor(user);
-			model.addAttribute("instructor", instr);
-		} else {
-			Client client = service.getClient(user);
-			model.addAttribute("client", client);
-		}
+		if (user.getType() == 0) return "redirect:/admin.html";
+		if (user.getType() == 1) model.addAttribute("instructor", service.getInstructor(user));
+		if (user.getType() == 2) model.addAttribute("instructor", service.getClient(user));
 
 		model.addAttribute("doublepassword", new DoublePassword());
 		return "account";
 		
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
-	public String updateUser(Model model, HttpSession session, @ModelAttribute("client") Client client, @ModelAttribute("doublepassword") DoublePassword doublepassword, @ModelAttribute("instructor") Instructor instructor, BindingResult bindingResult){
+	@RequestMapping(value="/updateClient", method=RequestMethod.POST)
+	public String updateClientPage(@ModelAttribute("client") Client client, BindingResult bindingResult){
+		new ClientValidator().validate(client, bindingResult);
+		if(bindingResult.hasErrors()) return "account";
+		service.updateClient(client);
 		
-		System.out.println(service);
-		if (!client.isEmpty()) {
-
-			new ClientValidator().validate(client, bindingResult);		
-			if(bindingResult.hasErrors()) {
-				model.addAttribute("error", 1);
-				return "account";
-			}
-			service.updateClient(client);
-			model.addAttribute("client", client);
-		}
-		
-		if (!instructor.isEmpty()) {
-			System.out.println("instr");
-			new InstructorValidator().validate(client, bindingResult);
-			if (bindingResult.hasErrors()) {
-				model.addAttribute("error", 2);
-				return "account";
-			}
-			service.updateInstructor(instructor);
-			model.addAttribute("instructor", instructor);
-		}
-		
-		if (!doublepassword.isEmpty()) {
-			System.out.println("pass");
-			new DoublePasswordValidator().validate(doublepassword, bindingResult);
-			if (bindingResult.hasErrors()) {
-				model.addAttribute("error", 3);
-				return "account";
-			}
-			User user = (User) session.getAttribute("user");
-			user.setPassword(doublepassword.getPassword());
-			service.updateUserWithPasswordType(user);
-		}
-		
-		model.addAttribute("error", 0);
-		return "/account";
+		return "redirect:/account.html";
 	}
+	
+	@RequestMapping(value="/updateInstructor", method=RequestMethod.POST)
+	public String updateInstructorPage(@ModelAttribute("instructor") Instructor instructor, BindingResult bindingResult){
+		new InstructorValidator().validate(instructor, bindingResult);
+		if(bindingResult.hasErrors()) return "account";
+		service.updateInstructor(instructor);
+		
+		return "redirect:/account.html";
+	}
+	
+	@RequestMapping(value="/updateAuth", method=RequestMethod.POST)
+	public String updateAuthPage(@ModelAttribute("doublepassword") DoublePassword doublePasswd, BindingResult bindingResult, HttpServletRequest request){
+		//TODO UserValidator
+		new DoublePasswordValidator().validate(doublePasswd, bindingResult);
+		if(bindingResult.hasErrors()) return "account";
+		
+		User user = (User) request.getSession().getAttribute("user");
+		user.setPassword(doublePasswd.getPassword());
+		service.updateUser(user);
+		
+		
+		return "redirect:/account.html";
+	}
+	
+	
+//	@RequestMapping(method=RequestMethod.POST)
+//	public String updateUser(Model model, HttpSession session, @ModelAttribute("client") Client client, @ModelAttribute("doublepassword") DoublePassword doublepassword, @ModelAttribute("instructor") Instructor instructor, BindingResult bindingResult){
+//		
+//		System.out.println(service);
+//		if (!client.isEmpty()) {
+//
+//			new ClientValidator().validate(client, bindingResult);		
+//			if(bindingResult.hasErrors()) {
+//				model.addAttribute("error", 1);
+//				return "account";
+//			}
+//			service.updateClient(client);
+//			model.addAttribute("client", client);
+//		}
+//		
+//		if (!instructor.isEmpty()) {
+//			System.out.println("instr");
+//			new InstructorValidator().validate(client, bindingResult);
+//			if (bindingResult.hasErrors()) {
+//				model.addAttribute("error", 2);
+//				return "account";
+//			}
+//			service.updateInstructor(instructor);
+//			model.addAttribute("instructor", instructor);
+//		}
+//		
+//		if (!doublepassword.isEmpty()) {
+//			System.out.println("pass");
+//			new DoublePasswordValidator().validate(doublepassword, bindingResult);
+//			if (bindingResult.hasErrors()) {
+//				model.addAttribute("error", 3);
+//				return "account";
+//			}
+//			User user = (User) session.getAttribute("user");
+//			user.setPassword(doublepassword.getPassword());
+//			service.updateUserWithPasswordType(user);
+//		}
+//		
+//		model.addAttribute("error", 0);
+//		return "/account";
+//	}
 
 }
