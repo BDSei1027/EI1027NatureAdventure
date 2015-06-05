3794411205f6c108abbf52eb002eaed64ca24137
RataDP <ratadp@gmail.com>
2015-06-05 12:35:11 +0200

    Funcionalidad para la pagina correctamente
    Signed-off-by: RataDP <ratadp@gmail.com>

--- EI1027NatureAdventure1/src/controller/adminPages/ClientManagement.java
+++ EI1027NatureAdventure1/src/controller/adminPages/ClientManagement.java
@@ -2,9 +2,16 @@ package controller.adminPages;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import validators.ClientValidator;
+import validators.EmailValidator;
+import classes.Client;
+import classes.Email;
 import controller.basics.AbstractController;
 
 
@@ -21,8 +28,38 @@ public class ClientManagement extends AbstractController {
 	
 	@RequestMapping(value="/details/{idClient}")
 	public String clientDetailPage(@PathVariable String idClient, Model model){
-		model.addAttribute("client", service.getClient(idClient));
+		Client client = service.getClient(idClient);
+		Email email = new Email();
+		email.setTo(client.getClientEmail());
+		
+		model.addAttribute("client", client);
+		model.addAttribute("email", email);
+		
 		return "admin/clientManagement/details";
+	}
+	
+	@RequestMapping(value="/details/{idClient}", method=RequestMethod.POST)
+	public String clientDetailPagePost(@PathVariable String idClient, @ModelAttribute("client") Client client, @ModelAttribute("email") Email email, BindingResult bindingResult, Model model) {
+		if (!client.isEmpty()) {
+			new ClientValidator().validate(client, bindingResult);
+			if (bindingResult.hasErrors()) return "admin/clientManagement/details";
+			service.updateClient(client);
+			System.out.println("updated");
+			return "redirect:/admin/clientManagement.html";
+		}
+		
+		if (!email.isEmpty()) {
+			new EmailValidator().validate(email, bindingResult);
+			if (bindingResult.hasErrors()) {
+				model.addAttribute("error", 1);
+				return "admin/clientManagement/details";
+			}
+			//service.sendMail(email);
+			model.addAttribute("error", 0);
+		}
+		
+		model.addAttribute("client", client);
+		return "/admin/clientManagement/details";
 	}
 	
 }
