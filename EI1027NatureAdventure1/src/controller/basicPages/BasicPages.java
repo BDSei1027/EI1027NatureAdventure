233b4ae488c170bbc565adfb855e054155e9a11a
Demils <Demils@Demils-PC>
2015-06-07 20:18:33 +0200

    pushed

--- EI1027NatureAdventure1/src/controller/basicPages/BasicPages.java
+++ EI1027NatureAdventure1/src/controller/basicPages/BasicPages.java
@@ -1,6 +1,7 @@
 package controller.basicPages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import controller.basics.AbstractController;
@@ -16,7 +17,8 @@ public class BasicPages extends AbstractController {
 	
 	
 	@RequestMapping(value="/activities")
-	public String activitiesPage(){
+	public String activitiesPage(Model model){
+		model.addAttribute("activityList", service.getAllActivities());
 		return "activities";
 	}
 	
