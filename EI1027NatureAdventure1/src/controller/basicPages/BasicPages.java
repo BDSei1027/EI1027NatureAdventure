652e5157fce7c21a5497b0b5c93b8bc297fe51f5
Demils <Demils@Demils-PC>
2015-06-07 20:47:46 +0200

    push

--- EI1027NatureAdventure1/src/controller/basicPages/BasicPages.java
+++ EI1027NatureAdventure1/src/controller/basicPages/BasicPages.java
@@ -1,9 +1,16 @@
 package controller.basicPages;
 
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import classes.Activity;
+import classes.Booking;
+import classes.Client;
 import controller.basics.AbstractController;
 
 
@@ -20,6 +27,22 @@ public class BasicPages extends AbstractController {
 	public String activitiesPage(Model model){
 		model.addAttribute("activityList", service.getAllActivities());
 		return "activities";
+	}
+	
+	@RequestMapping(value="/activities/newBooking/{idAct}")
+	public String newBookingPage(Locale locale, @PathVariable int idAct, Model model){
+		Booking booking = new Booking();
+		booking.setIdAct(idAct);
+		booking.setDateCreation(new Date());
+		
+		Activity act = service.getActivity(idAct);
+		
+		model.addAttribute("client", new Client());
+		model.addAttribute("booking", booking);
+		if(locale.getLanguage().equalsIgnoreCase("en")) model.addAttribute("actName", act.getName());
+		else model.addAttribute("actName", act.getNombre());
+		
+		return "activities/newBooking";
 	}
 	
 	@RequestMapping(value="/about")
