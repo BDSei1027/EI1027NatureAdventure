016046d6ece84a31cd581d1a0ab3bfb5e48dff93
Demils <Demils@Demils-PC>
2015-07-16 08:18:45 +0200

    push + vaya tonteria

--- EI1027NatureAdventure1/src/controller/adminPages/MainAdmin.java
+++ EI1027NatureAdventure1/src/controller/adminPages/MainAdmin.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import validators.DoublePasswordValidator;
+import validators.NoteValidator;
 import classes.DoublePassword;
 import classes.Note;
 import classes.User;
